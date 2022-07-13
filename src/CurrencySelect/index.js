602ea1dadf72c162173ea8c2ea8399b436354e87
Karol Galera <frontend.galera@gmail.com>
2022-07-13 20:35:54 +0200

    shortname added to select options

--- src/CurrencySelect/index.js
+++ src/CurrencySelect/index.js
@@ -15,9 +15,9 @@ const CurrencySelect = ({ currencies }) => {
                 onChange={({target}) => setCurrencyName(target.value)}
                 className="label__field">
                 <option />
-                <option> {currencies[0].name} </option>
-                <option> {currencies[1].name} </option>
-                <option> {currencies[2].name} </option>
+                <option> {currencies[0].name} - {currencies[0].shortName} </option>
+                <option> {currencies[1].name} - {currencies[1].shortName}</option>
+                <option> {currencies[2].name} - {currencies[2].shortName}</option>
             </select>
         </label>
     )
