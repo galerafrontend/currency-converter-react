56090d5b11ed92b92f6fcd812ae150187d076ef8
Karol Galera <frontend.galera@gmail.com>
2022-08-07 21:21:06 +0200

    styled-components added to Clock component

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
-import "./style.css";
+import { Date } from "./styled";
 
 const formattedDate = (date) => date.toLocaleDateString(
   undefined, {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  }
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+}
 );
 
 const formattedTime = (date) => date.toLocaleTimeString(
   undefined, {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  }
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+}
 );
-
 
 const Clock = () => {
   const [date, setDate] = useState(new Date());
@@ -32,11 +31,11 @@ const Clock = () => {
   }, []);
 
   return (
-    <div className="clock">
+    <Date>
       Dzisiaj jest
       {" "}
       {formattedDate(date)},&nbsp;{formattedTime(date)}
-    </div>
+    </Date>
   )
 };
 
--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -1,8 +1,10 @@
-.clock {
+import styled from "styled-components";
+
+export const Date = styled.div`
   display: flex;
   justify-content: end;
   font-family: 'Roboto Mono', monospace;
   font-size: 15px;
   color: hsl(0, 0%, 57%);
   padding-top: 10px;
-}
+`;
