d4245d7e8eea71c2f9c4ef28bb84e1549ac26694
Karol Galera <frontend.galera@gmail.com>
2022-08-26 22:29:57 +0200

    component LoadingText added to Form

---
+++ src/Form/LoadingText/index.js
@@ -0,0 +1,11 @@
+import { LoadingInfo } from "./styled";
+
+const LoadingText = () => {
+  return (
+    <LoadingInfo>
+      Trwa ładowanie danych z Europejskiego Banku Centralnego ... 😁
+    </LoadingInfo>
+  )
+};
+
+export default LoadingText;
---
+++ src/Form/LoadingText/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LoadingInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  color: teal;
+  padding-bottom: 15px;
+`;
